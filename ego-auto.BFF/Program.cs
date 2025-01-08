38fb5b4cdf2b3cf8c6e9b8edd6150a26babe5cd1
Olaru Andrei <66011172+olaruandrei1@users.noreply.github.com>
2025-01-08 19:36:34 +0200

    Add swagger Bearer token + config swagger

--- ego-auto.BFF/Program.cs
+++ ego-auto.BFF/Program.cs
@@ -1,47 +1,28 @@
-using ego_auto.BFF.Application;
-using ego_auto.BFF.Domain;
-using ego_auto.BFF.Domain.Common.Bindings;
-using ego_auto.BFF.Domain.ExceptionTypes;
-using ego_auto.BFF.Infrastructure;
 using ego_auto.BFF.Middleware;
-using ego_auto.BFF.Persistence;
+using ego_auto.BFF.Utilities;
 using Serilog;
 using Serilog.Formatting.Compact;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var appSettings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
-var connectionStrings = builder.Configuration.GetSection("ConnectionStrings").Get<ConnectionStrings>();
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.File(new CompactJsonFormatter(), "logs/{Date}.json", rollingInterval: RollingInterval.Day, fileSizeLimitBytes: 10_000_000, rollOnFileSizeLimit: true)
     .CreateLogger();
 
-BindConfigurationObjects.Register(builder.Services, builder.Configuration);
-ApplicationDependencies.Register(builder.Services);
-PersistenceDependencies.Register
-        (
-            builder.Services,
-            connectionStrings ?? throw new CustomNotFound("Connection strings properties are missing from configurable file.")
-        );
-InfrastructureDependencies.Register
-        (
-            builder.Services,
-            appSettings ?? throw new CustomNotFound("AppSettings properties are missing from configurable file.")
-        );
-AuthenticationDependencies.Configuration(builder.Services, appSettings.JwtConfiguration);
+DependencyOrchestration.Register(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ego.Auto.BFF-API");
+});
 
 app.UseMiddleware<TraceMiddleware>();
 
---
+++ ego-auto.BFF/Utilities/SwaggerConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace ego_auto.BFF.Utilities;
+
+public static class SwaggerConfiguration
+{
+    public static void AdditionalSettings(this IServiceCollection services)
+    => services.AddSwaggerGen
+                (
+                    c =>
+                        {
+                            c.SwaggerDoc("v1", new OpenApiInfo
+                            {
+                                Title = "Ego.Auto.BFF-API",
+                                Version = "v1",
+                                Description = "BackendForFrontend APIs destinated for ego-auto booking vehicles web application.",
+                                Contact = new OpenApiContact
+                                {
+                                    Name = "Ego Auto Support",
+                                    Email = "support@egoauto.com"
+                                }
+                            });
+
+                            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                            {
+                                Description = @"Enter jwt bearer:",
+                                Name = "Authorization",
+                                In = ParameterLocation.Header,
+                                Type = SecuritySchemeType.ApiKey,
+                                Scheme = "Bearer"
+                            });
+
+                            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                            {
+                                {
+                                    new OpenApiSecurityScheme
+                                    {
+                                        Reference = new OpenApiReference
+                                        {
+                                            Type = ReferenceType.SecurityScheme,
+                                            Id = "Bearer"
+                                        },
+                                        Scheme = "oauth2",
+                                        Name = "Bearer",
+                                        In = ParameterLocation.Header,
+                                    },
+                                    new List<string>()
+                                }
+                            });
+                        }
+                );
+}
